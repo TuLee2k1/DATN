93ffdd07878f90274b5ddb9a54f8d3a60c7c730b
TienHo21 <hotuyen21092004@gmail.com>
2024-12-11 16:04:28 +0700

    cap nhat thong ke home

--- src/main/java/poly/com/controller/HomeController.java
+++ src/main/java/poly/com/controller/HomeController.java
@@ -1,6 +1,7 @@
 package poly.com.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,13 +13,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import poly.com.dto.response.JobPost.JobListActiveResponse;
 import poly.com.dto.response.JobPost.JobListingResponse;
 import poly.com.model.JobPost;
+import poly.com.repository.CompanyRepository;
+import poly.com.repository.JobPostRepository;
+import poly.com.repository.ProfileRepository;
+import poly.com.repository.UserRepository;
 import poly.com.service.JobPostService;
+import poly.com.service.ThongkeService;
+
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("")
 public class HomeController {
     private final JobPostService jobPostService;
+    @Autowired
+    ThongkeService thongkeService;
 
     @GetMapping("")
     public String getJobListingsByStatus(
@@ -39,6 +49,15 @@ public class HomeController {
         model.addAttribute("currentPage", pageNo);
         model.addAttribute("totalPages", jobListings.getTotalPages());
         model.addAttribute("status", status);
+
+        //thongke
+        Map<String, Long> stats = thongkeService.getStatistics();
+
+        // Truyền các số liệu thống kê vào model
+        model.addAttribute("totalUser", stats.get("totalUser"));
+        model.addAttribute("totalCompanies", stats.get("totalCompanies"));
+        model.addAttribute("totalJobPosts", stats.get("totalJobPosts"));
+        model.addAttribute("totalJobProfiles", stats.get("totalJobProfiles"));
 
         return "fragments/home";
     }
